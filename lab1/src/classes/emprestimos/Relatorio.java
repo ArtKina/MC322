db5cffbc5c6c06358ec0cdf5e9a58aef4c4c4e17
ArthurKina <tutikina@gmail.com>
2023-08-27 14:59:05 -0300

    adicionei atributos, getters e construtor para relatorio e renovacaoreserva

--- lab1/src/classes/emprestimos/Relatorio.java
+++ lab1/src/classes/emprestimos/Relatorio.java
@@ -1,4 +1,19 @@
 package classes.emprestimos;
 
 public class Relatorio {
+    private String relatorioMultas;
+    private String estatisticas;
+    
+    public Relatorio(String relatorioMultas, String estatisticas) {
+    	this.relatorioMultas = relatorioMultas;
+    	this.estatisticas = estatisticas;
+    }
+    
+    public String getRelatorioMultas() {
+    	return relatorioMultas;
+    }
+    
+    public String getEstatisticas() {
+    	return estatisticas;
+    }
 }
--- lab1/src/classes/emprestimos/RenovacaoReserva.java
+++ lab1/src/classes/emprestimos/RenovacaoReserva.java
@@ -1,4 +1,15 @@
 package classes.emprestimos;
 
+import classes.multimidias.Multimidia;
+
 public class RenovacaoReserva {
+	private Multimidia item;
+	
+	public RenovacaoReserva(Multimidia item) {
+		this.item = item;
+	}
+	
+	public Multimidia getItem() {
+		return item;
+	}
 }
