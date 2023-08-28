2cd7c00e46a95dc3c6ed91b780464e32a7e43fef
george-sa <georgesa201097@gmail.com>
2023-08-27 22:03:26 -0300

    Implementei a relação de agregação na classe membro

--- lab1/src/classes/membros/Membro.java
+++ lab1/src/classes/membros/Membro.java
@@ -6,6 +6,10 @@ public class Membro {
     private String id;
     private String nome;
     private Emprestimo[] emprestimos;//relação de associacao entre membro e emprestimo
+    private Funcionario[] funcionarios; //Agregação com a classe Funcionario
+    private Graduando[] graduandos; //Agregação com a classe Graduando
+    private PosGraduando[] posGraduandos; //Agregação com a classe PosGraduando
+    private Professor[] professors; //Agregação com a classe Professor
     private int numEmprestimos;
     
     //Construtor
@@ -14,6 +18,29 @@ public class Membro {
         this.nome = nome;
         emprestimos = new Emprestimo[maxEmprestimos];
         numEmprestimos = 0;
+    }
+    // Construtor para Funcionário
+    public Membro(String id, String nome, int maxEmprestimos, Funcionario[] funcionarios) {
+        this(id, nome, maxEmprestimos);
+        this.funcionarios = funcionarios;
+    }
+
+    // Construtor para Graduando
+    public Membro(String id, String nome, int maxEmprestimos, Graduando[] graduandos) {
+        this(id, nome, maxEmprestimos);
+        this.graduandos = graduandos;
+    }
+
+    // Construtor para Pós-Graduando
+    public Membro(String id, String nome, int maxEmprestimos, PosGraduando[] posGraduandos) {
+        this(id, nome, maxEmprestimos);
+        this.posGraduandos = posGraduandos;
+    }
+
+    // Construtor para Professor
+    public Membro(String id, String nome, int maxEmprestimos, Professor[] professors) {
+        this(id, nome, maxEmprestimos);
+        this.professors = professors;
     }
 
     //Getters para acessar os atributos privados
@@ -33,4 +60,41 @@ public class Membro {
     		System.out.println("Você atingiu o número máximo de empréstimos");
     	}
     }
+
+    // Getter e Setter para Funcionarios
+    public Funcionario[] getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(Funcionario[] funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
+    // Getter e Setter para Graduandos
+    public Graduando[] getGraduandos() {
+        return graduandos;
+    }
+
+    public void setGraduandos(Graduando[] graduandos) {
+        this.graduandos = graduandos;
+    }
+
+    // Getter e Setter para PosGraduandos
+    public PosGraduando[] getPosGraduandos() {
+        return posGraduandos;
+    }
+
+    public void setPosGraduandos(PosGraduando[] posGraduandos) {
+        this.posGraduandos = posGraduandos;
+    }
+
+    // Getter e Setter para Professors
+    public Professor[] getProfessors() {
+        return professors;
+    }
+
+    public void setProfessors(Professor[] professors) {
+        this.professors = professors;
+    }
+
 }
