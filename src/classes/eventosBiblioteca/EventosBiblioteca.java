ede3e37d53e1c7d5a1b9aa606d8710abed1d8f7c
george-sa <georgesa201097@gmail.com>
2023-09-10 19:26:40 -0300

    Adicionando as classes internas e o ENUM

---
+++ src/classes/eventosBiblioteca/EventosBiblioteca.java
@@ -0,0 +1,193 @@
+package classes.eventosBiblioteca;
+
+public class EventosBiblioteca {
+    private static String nomeEvento;
+
+    public EventosBiblioteca(String nomeEvento) {
+        this.nomeEvento = nomeEvento;
+    }
+
+    public static String getNomeEvento() {
+        return nomeEvento;
+    }
+
+    public static void setNomeEvento(String nomeEvento) {
+        EventosBiblioteca.nomeEvento = nomeEvento;
+    }
+
+    //Classe Interna Palestra
+    public class Palestra {
+        public String palestrante;
+        public String topico;
+        public String data;
+        public String horario;
+        public String local;
+
+        public Palestra(String palestrante, String topico, String data, String horario, String local) {
+            this.palestrante = palestrante;
+            this.topico = topico;
+            this.data = data;
+            this.horario = horario;
+            this.local = local;
+        }
+
+        public String getPalestrante() {
+            return palestrante;
+        }
+
+        public void setPalestrante(String palestrante) {
+            this.palestrante = palestrante;
+        }
+
+        public String getTopico() {
+            return topico;
+        }
+
+        public void setTopico(String topico) {
+            this.topico = topico;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public String getHorario() {
+            return horario;
+        }
+
+        public void setHorario(String horario) {
+            this.horario = horario;
+        }
+
+        public String getLocal() {
+            return local;
+        }
+
+        public void setLocal(String local) {
+            this.local = local;
+        }
+    }
+
+    //Classe Interna Workshop
+    public class Workshop {
+        public String instrutor;
+        public String topico;
+        public String materiaisNecessarios;
+        public String data;
+        public String horario;
+        public String local;
+
+        public Workshop(String instrutor, String topico, String materiaisNecessarios, String data, String horario, String local) {
+            this.instrutor = instrutor;
+            this.topico = topico;
+            this.materiaisNecessarios = materiaisNecessarios;
+            this.data = data;
+            this.horario = horario;
+            this.local = local;
+        }
+
+        public String getInstrutor() {
+            return instrutor;
+        }
+
+        public void setInstrutor(String instrutor) {
+            this.instrutor = instrutor;
+        }
+
+        public String getTopico() {
+            return topico;
+        }
+
+        public void setTopico(String topico) {
+            this.topico = topico;
+        }
+
+        public String getMateriaisNecessarios() {
+            return materiaisNecessarios;
+        }
+
+        public void setMateriaisNecessarios(String materiaisNecessarios) {
+            this.materiaisNecessarios = materiaisNecessarios;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public String getHorario() {
+            return horario;
+        }
+
+        public void setHorario(String horario) {
+            this.horario = horario;
+        }
+
+        public String getLocal() {
+            return local;
+        }
+
+        public void setLocal(String local) {
+            this.local = local;
+        }
+    }
+
+    //Classe Interna Exposicao
+    public class Exposicao {
+        public String tema;
+        public String expositores;
+        public String duracao;
+        public String local;
+
+        public Exposicao(String tema, String expositores, String duracao, String local) {
+            this.tema = tema;
+            this.expositores = expositores;
+            this.duracao = duracao;
+            this.local = local;
+        }
+
+        public String getTema() {
+            return tema;
+        }
+
+        public void setTema(String tema) {
+            this.tema = tema;
+        }
+
+        public String getExpositores() {
+            return expositores;
+        }
+
+        public void setExpositores(String expositores) {
+            this.expositores = expositores;
+        }
+
+        public String getDuracao() {
+            return duracao;
+        }
+
+        public void setDuracao(String duracao) {
+            this.duracao = duracao;
+        }
+
+        public String getLocal() {
+            return local;
+        }
+
+        public void setLocal(String local) {
+            this.local = local;
+        }
+    }
+
+    // Enum: TipoDeEvento
+    public enum TipoDeEvento {
+        PALESTRA, WORKSHOP, EXPOSIÇÃO
+    }
+}
---
+++ src/main/BibliotecaMain.java
@@ -0,0 +1,41 @@
+import classes.emprestimos.*;
+import classes.membros.*;
+import classes.multimidias.*;
+import classes.equipamentos.*;
+
+public class main {
+    public static void main(String[] args) {
+        LivroFisico livro = new LivroFisico("Dom Quixote", "Miguel De Cervantes", "Francisco de Robles", 1605, "Aventura", "A história relata os feitos de um ingênuo e fidalgo cavaleiro medieval", null, "9788408061052", 1, 3, 1, "1a estante", "baixa");
+        String titulo = livro.getTitulo();
+        System.out.println(titulo);
+
+        //Implementação das classes internas e do ENUM para a classe Equipamentos
+        
+        Equipamentos equipamentosBiblioteca = new Equipamentos("Equipamento para o evento clube da leitura");
+
+        Equipamentos.Informatica computador = equipamentosBiblioteca.new Informatica("Computador", "Dell", "Optiplex 3050");
+        Equipamentos.Audiovisual camera = equipamentosBiblioteca.new Audiovisual("Câmera", "Sony", "Alpha 6400");
+        Equipamentos.Impressao impressora = equipamentosBiblioteca.new Impressao("Impressora", "HP", "LaserJet Pro MFP M130nw");
+
+        System.out.println("Detalhes do Equipamento de Informática:");
+        System.out.println("Tipo: " + computador.tipo);
+        System.out.println("Marca: " + computador.marca);
+        System.out.println("Modelo: " + computador.modelo);
+        System.out.println("Categoria: " + Equipamentos.CategoriaEquipamento.INFORMATICA);
+        System.out.println();
+
+        System.out.println("Detalhes do Equipamento Audiovisual:");
+        System.out.println("Tipo: " + camera.tipo);
+        System.out.println("Marca: " + camera.marca);
+        System.out.println("Modelo: " + camera.modelo);
+        System.out.println("Categoria: " + Equipamentos.CategoriaEquipamento.AUDIOVISUAL);
+        System.out.println();
+
+        System.out.println("Detalhes do Equipamento de Impressão:");
+        System.out.println("Tipo: " + impressora.tipo);
+        System.out.println("Marca: " + impressora.marca);
+        System.out.println("Modelo: " + impressora.modelo);
+        System.out.println("Categoria: " + Equipamentos.CategoriaEquipamento.IMPRESSÃO);
+
+    }
+}
