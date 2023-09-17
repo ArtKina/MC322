3ebff13ce6539532fbac4c0fa43385538d461508
ArthurKina <tutikina@gmail.com>
2023-09-17 18:57:20 -0300

    juntei a estrutura modelo com as classes internas adicionadas semana passada

---
+++ src/biblioteca/controllers/Equipamentos.java
@@ -0,0 +1,137 @@
+package biblioteca.controllers;
+
+public class Equipamentos {
+    private static String nomeEquipamentos;
+
+    public Equipamentos(String nomeEquipamentos) {
+        this.nomeEquipamentos = nomeEquipamentos;
+    }
+
+    // Getter e Setter para nomeEquipamentos
+    public static String getNomeEquipamentos() {
+        return nomeEquipamentos;
+    }
+
+    public static void setNomeEquipamentos(String nomeEquipamentos) {
+        Equipamentos.nomeEquipamentos = nomeEquipamentos;
+    }
+
+    // Classe Interna Informatica
+    public class Informatica {
+        public String tipo;
+        public String marca;
+        public String modelo;
+
+        public Informatica(String tipo, String marca, String modelo) {
+            this.tipo = tipo;
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+
+        // Getters e Setters para Informatica
+        public String getTipo() {
+            return tipo;
+        }
+
+        public void setTipo(String tipo) {
+            this.tipo = tipo;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+    }
+
+    //Classe Interna Audiovisual
+    public class Audiovisual {
+        public String tipo;
+        public String marca;
+        public String modelo;
+
+        public Audiovisual(String tipo, String marca, String modelo) {
+            this.tipo = tipo;
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+
+        // Getters e Setters para Audiovisual
+        public String getTipo() {
+            return tipo;
+        }
+
+        public void setTipo(String tipo) {
+            this.tipo = tipo;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+    }
+
+    //Classe Interna Impressao
+    public class Impressao {
+        public String tipo;
+        public String marca;
+        public String modelo;
+
+        public Impressao(String tipo, String marca, String modelo) {
+            this.tipo = tipo;
+            this.marca = marca;
+            this.modelo = modelo;
+        }
+
+        // Getters e Setters para Impressao
+        public String getTipo() {
+            return tipo;
+        }
+
+        public void setTipo(String tipo) {
+            this.tipo = tipo;
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public void setMarca(String marca) {
+            this.marca = marca;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+    }
+
+    // Enum: CategoriaEquipamento
+    public enum CategoriaEquipamento {
+        INFORMATICA, AUDIOVISUAL, IMPRESSÃO
+    }
+}
--- src/biblioteca/controllers/multimidias/Multimidia.java
+++ src/biblioteca/controllers/multimidias/Multimidia.java
@@ -28,9 +28,88 @@ public class Multimidia {
     public String getAutor() {
         return autor;
     }
-    
+        //Classe Interna Audio
+				public class Audio {
+					public String titulo;
+					public String autor;
+	
+					public Audio(String titulo, String autor) {
+							this.titulo = titulo;
+							this.autor = autor;
+					}
+	
+					// Getter para o título do áudio interno
+					public String getTitulo() {
+							return titulo;
+					}
+	
+					// Setter para o título do áudio interno
+					public void setTitulo(String titulo) {
+							this.titulo = titulo;
+					}
+	
+					// Getter para o autor do áudio interno
+					public String getAutor() {
+							return autor;
+					}
+	
+					// Setter para o autor do áudio interno
+					public void setAutor(String autor) {
+							this.autor = autor;
+					}
+			}
+	
+			//Classe Interna Video
+			public class Video {
+					public String titulo;
+					public String autor;
+	
+					public Video(String titulo, String autor) {
+							this.titulo = titulo;
+							this.autor = autor;
+					}
+	
+					// Getter para o título do vídeo interno
+					public String getTitulo() {
+							return titulo;
+					}
+	
+					// Setter para o título do vídeo interno
+					public void setTitulo(String titulo) {
+							this.titulo = titulo;
+					}
+	
+					// Getter para o autor do vídeo interno
+					public String getAutor() {
+							return autor;
+					}
+	
+					// Setter para o autor do vídeo interno
+					public void setAutor(String autor) {
+							this.autor = autor;
+					}
+			}
+	
+			//Classe Interna Software
+			public class Software {
+					public String titulo;
+	
+					public Software(String titulo) {
+							this.titulo = titulo;
+					}
+	
+					// Getter para o título do software interno
+					public String getTitulo() {
+							return titulo;
+					}
+	
+					// Setter para o título do software interno
+					public void setTitulo(String titulo) {
+							this.titulo = titulo;
+					}
     //Método
     public void determinarEmprestimo(){
         System.out.println("O item multimidia foi emprestado!");
     }
+	}
 }
